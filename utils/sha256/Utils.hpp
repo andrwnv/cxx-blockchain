f1b74575ae1793f8ec4b1a32231c552816527843
Andrew G <glazynovand@gmail.com>
2022-11-10 00:18:50 +0700

    sfinae fix

--- utils/sha256/Utils.hpp
+++ utils/sha256/Utils.hpp
@@ -2,29 +2,12 @@
 
 #include <type_traits>
 #include <cstdint>
-
-namespace std
-{
-
-template <bool Disable, typename Type = void>
-using disable_if = std::enable_if<not Disable, Type>;
-
-template <bool Disable, typename Type = void>
-using disable_if_t = std::disable_if<Disable, Type>::type;
-
-}
-
 namespace sha256::rotation
 {
 
 template<
     class Type,
     std::enable_if_t<std::is_unsigned_v<Type>, bool> = true,
-
-    std::disable_if_t<std::is_same_v<Type, float>, bool> = true,
-    std::disable_if_t<std::is_same_v<Type, double>, bool> = true,
-    std::disable_if_t<std::is_same_v<Type, long double>, bool> = true,
-
     uint8_t TypeSize = sizeof(Type) * 8
 > 
 inline Type rightRotate(Type value, uint8_t rotateBy) noexcept
@@ -35,11 +18,6 @@ inline Type rightRotate(Type value, uint8_t rotateBy) noexcept
 template<
     class Type,
     std::enable_if_t<std::is_unsigned_v<Type>, bool> = true,
-
-    std::disable_if_t<std::is_same_v<Type, float>, bool> = true,
-    std::disable_if_t<std::is_same_v<Type, double>, bool> = true,
-    std::disable_if_t<std::is_same_v<Type, long double>, bool> = true,
-
     uint8_t TypeSize = sizeof(Type) * 8
 > 
 inline Type leftRotate(Type value, uint8_t rotateBy) noexcept
